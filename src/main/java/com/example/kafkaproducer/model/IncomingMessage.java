3b82dc680a2ed13b7d2a02c5c05d37140923ce09
Reinaldo Hermosilla <ghost_cad@hotmail.com>
2021-06-25 12:53:07 -0300

    created kafka producer service

---
+++ src/main/java/com/example/kafkaproducer/model/IncomingMessage.java
@@ -0,0 +1,11 @@
+package com.example.kafkaproducer.model;
+
+import lombok.Data;
+
+@Data
+public class IncomingMessage {
+
+    private String topic;
+    private String key;
+    private String value;
+}
