1a2ec58fb0d6cf2cf47a4241c2229dff85b33917
Reinaldo Hermosilla <ghost_cad@hotmail.com>
2021-06-28 09:41:13 -0300

    added KafkaMessage Controller, and Readme file

---
+++ README.md
@@ -0,0 +1,5 @@
+1-start confluent services
+2-create the kafka topic
+3-start the consumer
+4-execute app
+5-send a msg in postman, check consumer terminal
---
+++ src/main/java/com/example/kafkaproducer/services/KafkaMessageController.java
@@ -0,0 +1,20 @@
+package com.example.kafkaproducer.services;
+
+import com.example.kafkaproducer.model.IncomingMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KafkaMessageController {
+
+    @Autowired KafkaProducerService kafkaProducerService;
+
+    @PostMapping("/post")
+    public String sendMessageToKafka(@RequestBody IncomingMessage message){
+        kafkaProducerService.sendMessage(message.getTopic(), message.getKey(),message.getValue());
+        return String.format("Success - Message for key %s is sent to Kafka Topic: %s",
+                message.getKey(), message.getTopic());
+    }
+}
